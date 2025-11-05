8c8c994418fff72a383935a232f6372407962154
ricardbenavides <129066993+ricardbenavides@users.noreply.github.com>
2025-11-05 17:27:16 -0300

    Update locals.tf

--- locals.tf
+++ locals.tf
@@ -1,12 +1,1 @@
 
-locals {
-  tags_default = {
-    pais          = var.pais
-    cliente       = var.cliente
-    ambiente      = var.ambiente
-    area          = var.area
-    owner         = var.owner
-    clasificacion = var.clasificacion
-  }
-
-}
